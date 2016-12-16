9b9ce30204353960038660f58b08b3107c604714
Thomas Kerbrat <kerbrat@outlook.com>
2016-12-16 18:31:04 +0100

    First implementation of setItem.

--- src/json-persistent-storage.js
+++ src/json-persistent-storage.js
@@ -1,11 +1,19 @@
 (function (exports) {
 
-    function JsonPersistentStorage(path) {
+    var path = require('path');
+    var fs = require('fs');
+    var mkdirp = require('mkdirp');
+
+    var noop = function () { };
+
+    var cl = console.log;
+
+    function JsonPersistentStorage(_path) {
         if (!(this instanceof JsonPersistentStorage)) {
-            return new JsonPersistentStorage();
+            return new JsonPersistentStorage(_path);
         }
 
-        if (typeof path !== 'string') {
+        if (typeof _path !== 'string') {
             throw TypeError('Missing path argument');
         }
 
@@ -26,9 +34,46 @@
         }
 
         function key(index) { }
+
         function getItem(key) { }
-        function setItem(key, value) { }
+
+        function setItem(key, value, cb) {
+            if (typeof cb === 'undefined') { cb = noop; }
+            else if (typeof cb !== 'function') { throw new Error('cb must be a function'); }
+
+            var value;
+            try {
+                value = JSON.stringify(value);
+            } catch (err) {
+                cb(err);
+            }
+
+            fs.access(_path, function (err) {
+                if (err !== null) {
+                    if (err.code === 'ENOENT') {
+                        mkdirp(_path);
+                    } else {
+                        cb(err);
+                    }
+                }
+
+                var filePath = path.format({ dir: _path, name: key, ext: '.json' });
+                fs.writeFile(filePath, value, function (err) {
+                    if (err) {
+                        cb(err);
+                    } else {
+                        if (keys.indexOf(key) === -1) {
+                            keys.push(key);
+                            length++;
+                        }
+                        cb(null);
+                    }
+                });
+            });
+        }
+
         function removeItem(key) { }
+
         function clear() { }
     }
 
--- test/json-persistent-storage.test.js
+++ test/json-persistent-storage.test.js
@@ -1,97 +1,129 @@
 const assert = require('chai').assert;
-const JsonPersistentStorage = require('../src/json-persistent-storage.js').JsonPersistentStorage;
+const Storage = require('../src/json-persistent-storage.js').JsonPersistentStorage;
 
-describe('JsonPersistentStorage', () => {
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+
+const cl = console.log;
+function log(value) { console.log('**LOG**', value); return value; };
+
+describe('JsonPersistentStorage', function () {
     let storage;
-    const myPath = 'myPath';
+    const myPath = 'test/data/myPath';
 
-    it('should throw an error when no params are given', () => {
-        assert.throws(() => {
-            storage = new JsonPersistentStorage();
+    it('should throw an error when no params are given', function () {
+        assert.throws(function () {
+            storage = new Storage();
         }, TypeError, /Missing path argument/);
     });
 
-    it('should not throw an error when a param is given', () => {
-        assert.doesNotThrow(() => {
-            storage = new JsonPersistentStorage(myPath);
+    it('should not throw an error when a param is given', function () {
+        assert.doesNotThrow(function () {
+            storage = new Storage(myPath);
         }, TypeError, /Missing path argument/);
     });
 
-    it('should instantiate a new JsonPersistentStorage object', () => {
-        storage = new JsonPersistentStorage(myPath);
-        assert.instanceOf(storage, JsonPersistentStorage);
+    it('should instantiate a new JsonPersistentStorage object', function () {
+        storage = new Storage(myPath);
+        assert.instanceOf(storage, Storage);
     });
 
-    describe('#length', () => {
+    describe('#length', function () {
         let storage;
 
-        beforeEach(() => {
-            storage = new JsonPersistentStorage(myPath);
+        beforeEach(function () {
+            storage = new Storage(myPath);
         });
 
-        it('should be a number', () => {
+        it('should be a number', function () {
             assert.isNumber(storage.length);
         });
 
-        it('should be initialized to 0', () => {
+        it('should be initialized to 0', function () {
             assert.strictEqual(storage.length, 0);
         });
 
-        it('should be readonly', () => {
+        it('should be readonly', function () {
             storage.length = 1;
             assert.strictEqual(storage.length, 0);
         });
     });
 
-    describe('#key()', () => {
+    describe('#key()', function () {
 
     });
 
-    describe('#getItem()', () => {
+    describe('#getItem()', function () {
 
     });
 
-    describe('#setItem()', () => {
+    describe('#setItem()', function () {
         let storage;
 
-        beforeEach(() => {
-            storage = new JsonPersistentStorage(myPath);
+        beforeEach(function () {
+            storage = new Storage(myPath);
         });
 
-        it('should increase when setting a new item', () => {
-            storage.setItem('foo', 'bar');
-            assert.strictEqual(storage.length, 1);
+        it('should write a file for the given key at "myPath/key.json"', function (done) {
+            const fileName = 'foo';
+            const filePath = path.format({ dir: myPath, name: fileName, ext: '.json' });
+
+            storage.setItem(fileName, 'bar', onSetItemDone);
+
+            function onSetItemDone(err) {
+                assert.strictEqual(err, null);
+                fs.access(filePath, function (err) {
+                    assert.strictEqual(err, null);
+                    done();
+                });
+            }
         });
 
-        it('should not increase for items already present', () => {
-            storage.setItem('foo', 'bar');
-            storage.setItem('foo', 'bar');
-            assert.strictEqual(storage.length, 1);
+        // Try to write a file but the name is already aken by a directory.
+        // Try to write a file into a subdir but the dir does not exist.
+
+        it('should increase length when setting a new item', function (done) {
+            storage.setItem('foo', 'bar', function (err) {
+                assert.strictEqual(err, null);
+                assert.strictEqual(storage.length, 1);
+                done();
+            });
+        });
+
+        it('should not increase length for items already present', function () {
+            storage.setItem('foo', 'bar', function (err) {
+                assert.strictEqual(err, null);
+                storage.setItem('foo', 'bar', function (err) {
+                    assert.strictEqual(err, null);
+                    assert.strictEqual(storage.length, 1);
+                });
+            });
         });
     });
 
-    describe('#removeItem()', () => {
+    describe('#removeItem()', function () {
         let storage;
 
-        beforeEach(() => {
-            storage = new JsonPersistentStorage(myPath);
+        beforeEach(function () {
+            storage = new Storage(myPath);
         });
 
-        it('should decrease when removing an item', () => {
+        it('should decrease when removing an item', function () {
             storage.setItem('foo', 'bar');
-            storage.removeitem('foo');
+            storage.removeItem('foo');
             assert.strictEqual(storage.length, 0);
         });
     });
 
-    describe('#clear()', () => {
+    describe('#clear()', function () {
         let storage;
 
-        beforeEach(() => {
-            storage = new JsonPersistentStorage(myPath);
+        beforeEach(function () {
+            storage = new Storage(myPath);
         });
 
-        it('should reset to 0 when clearing', () => {
+        it('should reset to 0 when clearing', function () {
             storage.setItem('foo', 'bar');
             assert.strictEqual(storage.length, 1);
 
